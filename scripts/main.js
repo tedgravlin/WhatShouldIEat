38a47b1e8fb44a561042f3d4a98cfbf4e2bff504
tedgravlin <123981212+tedgravlin@users.noreply.github.com>
2023-06-07 14:51:00 -0400

    Revert "Add alt text to images"
    This caused the spin animation to break. Will fix in later update.
    
    This reverts commit c6c897e8d60d10abfa9ddbdb85ccd9f96626bacf.

--- scripts/main.js
+++ scripts/main.js
@@ -67,7 +67,6 @@ function animateSpinWheel() {
         const icon = document.createElement("img");
         icon.className = "foodIcon";
         icon.setAttribute("src", "./assets/images/" + result + ".webp");
-        icon.setAttribute("alt", result);
         icon.id = "foodIcon";
 
         if (spinnerRevolutions == maxSpinnerRevolutions) {

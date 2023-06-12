8820fb6a9985a731f9546e41c60fe35dacaafebf
tedgravlin <123981212+tedgravlin@users.noreply.github.com>
2023-06-12 01:42:16 -0400

    Implement the ability to remove food options
    * Also adds an options menu
    
    This addresses #1

--- scripts/main.js
+++ scripts/main.js
@@ -18,8 +18,8 @@ function chooseRandomFood() {
     spin = Math.floor(Math.random() * foodList.length);
     result = foodList[spin];
 
-    // If lastResult isn't null
-    if (lastResult != null) {
+    // If lastResult isn't null and there's more than one item in foodList
+    if ((lastResult != null) && (foodList.length > 1)) {
         // If the result is the same as last time, keep spinning until it's different
         while (lastResult.toLowerCase() === result.toLowerCase()) {
             result = foodList[Math.floor(Math.random() * foodList.length)].toLowerCase();
@@ -111,4 +111,64 @@ function resetWheel() {
     openMapsButton.style.visibility = "hidden";
     // Make the bottom sheet slide down
     bottomSheet.style.animationName = "slideDown";
+}
+
+function openOptions() {
+    // Hide the spinner
+    document.getElementById("spinnerBorder").style.display = "none";
+    // Hide the buttons
+    document.getElementById("spinButton").style.display = "none";
+    document.getElementById("optionsButton").style.display = "none";
+    // Unhide the options menu
+    document.getElementById("optionsMenu").style.display = "block";
+
+    // Only generate items on first load
+    if (document.getElementById("optionsFoodList").childNodes.length <= 1) {
+        // Add all of the food items
+        for (var i = 0; i < foodList.length; i++) {
+            const foodLabel = document.createElement("p");
+            foodLabel.setAttribute("class", "optionsFoodLabel");
+            const removeButton = document.createElement("input");
+            removeButton.setAttribute("type", "button");
+            removeButton.setAttribute("value", "Remove");
+            removeButton.setAttribute("class", "removeButton");
+            removeButton.setAttribute("id", foodList[i]);
+            removeButton.addEventListener('click', function (e) {
+                handleRemovalButton(e.target.id);
+            }, false);
+            foodLabel.innerHTML = foodList[i];
+            // Load in the food items
+            document.getElementById("optionsFoodList").appendChild(foodLabel);
+            document.getElementById("optionsFoodList").appendChild(removeButton);
+            document.getElementById("optionsFoodList").appendChild(document.createElement("br"));
+        }
+    }
+}
+
+function exitOptions() {
+    // Unhide the spinner
+    document.getElementById("spinnerBorder").style.display = "block";
+    // Unide the buttons
+    document.getElementById("spinButton").style.display = "inline";
+    document.getElementById("optionsButton").style.display = "inline";
+    // Hide the options menu
+    document.getElementById("optionsMenu").style.display = "none";
+}
+
+function handleRemovalButton(item) {
+    // If the item is in the list, then remove it
+    if (foodList.includes(item)) {
+        const index = foodList.indexOf(item);
+        if (index > -1) {
+            foodList.splice(index, 1);
+        }
+        // Set the value of the button to "Include"
+        document.getElementById(item).setAttribute("value", "Include");
+    }
+    // If the item isn't in the list, add it to the list
+    else {
+        foodList.push(item);
+        // Set the value of the button to "Remove"
+        document.getElementById(item).setAttribute("value", "Remove");
+    }
 }
